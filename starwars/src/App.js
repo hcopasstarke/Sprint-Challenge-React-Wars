1b98f7fb3c4f486d9630e09faa54f546616c333c
Haili Copas-Starke <hcopasstarke@gmail.com>
2019-07-12 18:39:51 -0700

    working

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import "./App.css"
 
-import { Container, Divider } from 'semantic-ui-react';
+import { Container, Divider } from "semantic-ui-react"
 
-import {StarCharacterCompMap} from './components/StarCharacterCompMap';
-
+import { StarCharacterCompMap } from "./components/StarCharacterCompMap"
 
 const App = () => {
 	// Try to think through what state you'll need for this app before starting. Then build out
 	// the state properties here.
-	const [starChar, setStarChar] = useState([]);
+	const [starChar, setStarChar] = useState([])
 	console.log(starChar)
 
-	// Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+	// Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
 	// side effect in a component, you want to think about which state and/or props it should
 	// sync up with, if any.
 	useEffect(() => {
@@ -25,24 +24,24 @@ const App = () => {
 				setStarChar(res.data.results)
 			})
 			.catch(err => {
-				console.log(err);
-			});
+				console.log(err)
+			})
 	}, [])
 
 	return (
-		<div className="App">
-
+		<div className='App'>
 			<Container>
-				<h1 className="title"> React Wars: <br />M ay The Code Be With You ⚔︎</h1>
+				<h1 className='title'>
+					{" "}
+					React Wars: <br />
+					Dark or Light?
+				</h1>
 				<Divider />
 			</Container>
 
-
 			{starChar.length ? <StarCharacterCompMap characters={starChar} /> : <h1>loading</h1>}
-
 		</div>
-	);
+	)
 }
 
-
-export default App;
+export default App
